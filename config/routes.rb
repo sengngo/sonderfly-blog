8d493349c1faf102a7aabc8b06c30ef24650de75
Seng Ngo <sengngo@gmail.com>
2015-11-11 23:13:34 +0000

    added index post page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SonderflyBlog::Application.routes.draw do
+  root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
