167d19ec3174363dc632ee87b90c1f6f4c675af1
Seng Ngo <sengngo@gmail.com>
2015-11-12 03:09:30 +0000

    Adding user authentication to the application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 SonderflyBlog::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'sonderfly-blog-seng-ngo.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
